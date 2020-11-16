89ecd582f2d04b25bbb6fcd44a6ac57c0bc881c7
Lucas T <lucastamburlini@gmail.com>
2020-11-16 10:15:54 -0300

    v 0.0

---
+++ script.js
@@ -0,0 +1,17 @@
+let link1 = document.getElementById("link1");
+let section1 = document.getElementById("section1");
+link1.addEventListener("click", function () {
+    section1.scrollIntoView();
+});
+
+let link2 = document.getElementById("link2");
+let section2 = document.getElementById("section2");
+link2.addEventListener("click", function () {
+    section2.scrollIntoView();
+});
+
+let link3 = document.getElementById("link3");
+let section3 = document.getElementById("section3");
+link3.addEventListener("click", function () {
+    section3.scrollIntoView();
+});
